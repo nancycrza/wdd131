5db3cae13a4ac5ce19e6a4cfe3c69dc26d89f2be
nancycrza <nancycrzabyui@gmail.com>
2026-05-04 15:24:50 -0600

    changes

--- mission/user.js
+++ mission/user.js
@@ -1,24 +1,23 @@
-let selectElem = document.querySelector('#theme-selector');
-let logo = document.querySelector('.logo img');
+document.addEventListener('DOMContentLoaded', () => {
+  let selectElem = document.querySelector('#theme-selector');
+  let logo = document.querySelector('.logo img');
 
-selectElem.addEventListener('change', changeTheme);
+  selectElem.addEventListener('change', changeTheme);
 
-function changeTheme() {
-let current = selectElem.value;
+  function changeTheme() {
+    let current = selectElem.value;
 
-if (current == 'dark') {
-// dark grey theme
-document.body.classList.add('dark');
-document.body.classList.remove('light');
-logo.src = "https://wddbyui.github.io/wdd131/images/byui-logo-white.png";
-} else if (current == 'light') {
-// light theme
-document.body.classList.add('light');
-document.body.classList.remove('dark');
-logo.src = "https://wddbyui.github.io/wdd131/images/byui-logo-blue.png";
-} else {
-// default / "Choose Mode"
-document.body.classList.remove('dark', 'light');
-logo.src = "https://wddbyui.github.io/wdd131/images/byui-logo-blue.png";
-}
-}
+    if (current === 'dark') {
+      document.body.classList.add('dark');
+      document.body.classList.remove('light');
+      logo.src = "https://wdwdui.github.io/wdd131/images/byui-logo-white.png";
+    } else if (current === 'light') {
+      document.body.classList.add('light');
+      document.body.classList.remove('dark');
+      logo.src = "https://wdwdui.github.io/wdd131/images/byui-logo-blue.png";
+    } else {
+      document.body.classList.remove('dark', 'light');
+      logo.src = "https://wdwdui.github.io/wdd131/images/byui-logo-blue.png";
+    }
+  }
+});
