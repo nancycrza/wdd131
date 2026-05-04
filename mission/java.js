947a7541f99459c1c086ed9ebb8305b37d17bf7e
nancycrza <nancycrzabyui@gmail.com>
2026-05-04 12:53:25 -0600

    new

--- mission/java.js
+++ mission/java.js
@@ -9,11 +9,11 @@ function changeTheme() {
 
     if (current == 'dark') {
         // dark grey theme
-        document.body.className = 'dark-gray';
+        document.body.className = 'dark';
         logo.src = "https://wddbyui.github.io/wdd131/images/byui-logo-blue.webp"; //my light logo byu
     } else {
         // light grey theme
-        document.body.className = 'light-gray';
+        document.body.className = 'light';
         logo.src = "https://wddbyui.github.io/wdd131/images/byui-logo-white.png"; //my light logo byu
     }
 }           
